fe04e767347847cbceecb754c7e9f83b90cff987
Mayza Lutfi Setyaji <mayzalutfi656@gmail.com>
2025-01-18 12:41:21 +0700

    -Menambah kondisi ketika user menginput jumlah pesanan dibawah atau sama dengan 0
    -Menambah fitur untuk kondisi ketika user menginput uang dibawah atau sama dengan 0
    
    -Menambahkan beberapa comment sebagai keterangan

--- Vending.py
+++ Vending.py
@@ -1,17 +1,19 @@
 #Membuat kode program untuk sebuah Vending Machine
-def hitung_total_dan_kembalian(harga_satuan, uang, kembalian):
+def hitung_total_dan_kembalian(harga_satuan, uang, jumlah):
     try:
+        if harga_satuan <= 0 or jumlah <= 0:
+            return None, "Harga satuan dan jumlah tidak boleh kosong"
         total = harga_satuan * jumlah
         kembalian = uang - total
         if kembalian >= 0:
             return total, kembalian
-        else:
-            return None, "Uang anda tidak cukup"
+        else: #kondisi ketika kembalian memiliki nilai negatif
+            return None, "Uang anda tidak cukup" #None (tidak ada nilai yang dikembalikan)
     except ValueError:
         print("Masukkan angka yang valid untuk uang dan jumlah")
-        return None, None
+        return None, None #None 1(utk total) dan None 2(utk kembalian)
 
-#Daftar harga menggunakan dictionary
+#Daftar harga menggunakan dictionary dan tuple
 daftar_harga = {
     1: ("Mizone", 4000),
     2: ("Pocari", 6000),
@@ -22,7 +24,7 @@ daftar_harga = {
 while True:
     print("===SELAMAT DATANG DI VENDING MACHINE===")
     for k, v in daftar_harga.items():
-        print(f"{k}. {v[0]} = Rp.{v[1]}/pcs")
+        print(f"{k}. {v[0]} = Rp.{v[1]}/pcs") #k(key) dan v(value/produk dan harganya)
     print("5. Keluar")
 
     try:
@@ -32,15 +34,24 @@ while True:
             break
 
         minuman__pilihan, harga = daftar_harga.get(opsi, (None, None))
-        if opsi is None:
-            print("Pilihan tidak tersedia")
+        if opsi is None: #kondisi ketika user tidak mengisi opsi
+            print("Harap masukkan pilihan anda")
+        elif opsi not in daftar_harga: #kondisi ketika user memilih opsi yang tidak tersedia pada daftar_harga
+            print("Plih opsi tidak tersedia")
             continue
-
+    
         uang = float(input("Masukkan jumlah uang anda: "))
         jumlah = int(input("Masukkan jumlah barang yang dibeli: "))
         total, kembalian = hitung_total_dan_kembalian(harga, uang, jumlah)
         if total is not None:
             print(f"Total harga: {total}")
             print(f"Kembalian: {kembalian}")
+        elif uang <= 0:
+            print("Maaf, Vending Machine ini bukan tempatnya ngutang dawg")
+        elif jumlah <= 0:
+            print("Lo serius bro??")
+        
     except ValueError:
         print("Masukkan angka yang valid")    
+print()
+print(f"Terima kasih telah berbelanja")
